b6a26e9d2caab3b883d0c341b0c0def568aa685f
xqwtxon <136815140144@ncr2.deped.gov.ph>
2024-09-04 21:34:22 +0800

    Fix #52, Add todo: Glide check

--- src/utils/ReplaceText.php
+++ src/utils/ReplaceText.php
@@ -63,10 +63,10 @@ class ReplaceText extends ConfigManager {
 			$module,
 			$subType,
 			date("F d, Y h:i:sA", time()),
-			(is_string($player) ? (PlayerAPI::getAPIPlayer($player) === null ? "N/A" : PlayerAPI::getAPIPlayer($player)->getViolation()) : $player->getRealViolation($module)),
-			(is_string($player) ? (PlayerAPI::getAPIPlayer($player) === null ? "N/A" : PlayerAPI::getAPIPlayer($player)->getViolation()) : $player->getViolation($module)),
+			(is_string($player) ? (Server::getInstance()->getPlayerExact($player) === null ? "N/A" : PlayerAPI::getAPIPlayer($player)->getViolation()) : $player->getRealViolation($module)),
+			(is_string($player) ? (Server::getInstance()->getPlayerExact($player) === null ? "N/A" : PlayerAPI::getAPIPlayer($player)->getViolation()) : $player->getViolation($module)),
 			self::getData(self::CHAT_SPAM_DELAY),
-			(is_string($player) ? (PlayerAPI::getAPIPlayer($player) === null ? "N/A" : PlayerAPI::getAPIPlayer($player)->getCaptchaCode()) : $player->getCaptchaCode()),
+			(is_string($player) ? (Server::getInstance()->getPlayerExact($player) === null ? "N/A" : PlayerAPI::getAPIPlayer($player)->getCaptchaCode()) : $player->getCaptchaCode()),
 			Server::getInstance()->getTicksPerSecond(),
 			base64_encode(zlib_encode(strtolower($module . $subType . date("F d, Y h:i:sA", time())), ZLIB_ENCODING_DEFLATE, 9))
 		];
